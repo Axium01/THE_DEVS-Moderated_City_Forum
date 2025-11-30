057925b4383d64abfd3daa31e827cc4cc7c5ef8d
r-c-github <180685746+R-C-github@users.noreply.github.com>
2025-11-30 15:51:51 -0800

    Changes file paths to try and fix profile page
    Changes the file paths that check if the user is on the profile or index page.

--- js/script-profile.js
+++ js/script-profile.js
@@ -54,7 +54,7 @@ function showProfile() {
 
 // Check to see if it is a new user, and if they are on the homepage or profile page
 function checkPage() {
-  if (window.location.pathname == "/index.html") {
+  if (window.location.pathname == "index.html") {
     if (profile == null || profile.firstName == "") {
       document.getElementById("createProfile").style.display = "grid";
       document.getElementById("homepageContent").style.display = "none";
@@ -65,7 +65,7 @@ function checkPage() {
       document.getElementById("homepageContent").style.display = "grid";
     }
   }
-  else if (window.location.pathname == "/html/profile.html"){
+  else if (window.location.pathname == "../html/profile.html"){
     console.log(profile["firstName"]);
     if (profile != null) {
       showProfile();
