4de5da5636e5a75c0b2d94d5d4234294c36cd570
Milo Wearn <milo.wearn@bellevuecollege.edu>
2025-12-02 17:29:20 -0800

    Removed unnecessary functions

--- js/script-indexProfile.js
+++ js/script-indexProfile.js
@@ -1,29 +1,6 @@
 let profile = JSON.parse(localStorage.getItem('profile'));
-console.log("Script");
-checkPage();
 
-function loadProfile() {
-  document.getElementById("idexProfileForm").style.display = "grid";
-  document.getElementById("profileCard").style.display = "none";
-  if (JSON.parse(localStorage.getItem('profile')) != null)
-  {
-    document.forms["idexProfileForm"]["firstName"].value = profile.firstName;
-    document.forms["idexProfileForm"]["lastName"].value = profile.lastName;
-    document.forms["idexProfileForm"]["city"].value = profile.city;
-    document.forms["idexProfileForm"]["country"].value = profile.country;
-    document.forms["idexProfileForm"]["county"].value = profile.county;
-    document.forms["idexProfileForm"]["zip"].value = profile.zip;
-    document.forms["idexProfileForm"]["state"].value = profile.state;
-    console.log("Load profile");
-  }
-  else {
-    profile = {firstName:"", lastName:"", city:"", country:"", county:"", zip:"", state:""};
-    console.log("New profile");
-  }
-}
-
-function saveProfile()
-{
+function saveProfile() {
   // The user can only make a profile ONCE on the index page, so this shouldn't become an issue... I hope.
   profile = {firstName:"", lastName:"", city:"", country:"", county:"", zip:"", state:""};
   // Get values from input elements
@@ -41,21 +18,6 @@ function saveProfile()
   localStorage.setItem('profile', JSON.stringify(profile)); // Save profile by turning into string/JSON
 }
 
-
-function showProfile() {
-  document.getElementById("idexProfileForm").style.display = "none";
-  document.getElementById("profileCard").style.display = "grid";
-  
-  console.log(profile);
-
-  document.getElementById("nameP").innerHTML = "" + profile['firstName'] + " " + profile.lastName;
-  document.getElementById("cityP").innerHTML = "" + profile.city + " City,";
-  document.getElementById("countyP").innerHTML = "" + profile.county  + " County,";
-  document.getElementById("stateP").innerHTML = "" + profile.state + ",";
-  document.getElementById("countryP").innerHTML = "" + profile.country + ",";
-  document.getElementById("zipP").innerHTML = "" + profile.zip;
-}
-
 // Check to see if it is a new user, and if they are on the homepage or profile page
 function checkPage() {
   console.log("Checking page " + window.location.pathname);
@@ -69,6 +31,5 @@ function checkPage() {
     document.getElementById("homepageContent").style.display = "grid";
   }
 }
-
 
 checkPage();
